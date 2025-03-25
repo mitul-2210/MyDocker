0bd9e5eed5e37951608523e3732d22204db16670
mitul-2210 <149893329+mitul-2210@users.noreply.github.com>
2025-03-26 02:45:31 +0530

    Update app.py

--- Exp-2/app.py
+++ Exp-2/app.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
 import pandas as pd
+import os
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
